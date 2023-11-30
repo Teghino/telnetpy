0e45fc832c4d803d84864a23d17e202cb4778346
Teghino <tacchifra@gmail.com>
2023-11-30 10:13:10 +0100

    piccola modifica

--- client.py
+++ client.py
@@ -6,9 +6,14 @@ PORT = 23
 
 def run_client():
     with telnetlib.Telnet(HOST, PORT) as client:
+        response = client.read_until(b"\n")
+        print(response.decode('ascii'))
         while True:
             command = input("Inserisci un comando: ")
             if command.strip().lower() == "exit":
+                client.write((command + "\n").encode('ascii'))
+                response = client.read_until(b"\n")
+                print("Risposta dal server:", response.decode('ascii'))
                 break
             else:
                 client.write((command + "\n").encode('ascii'))
