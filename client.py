c8a08f59f77fd9dc2365ea41b02a0b77edc8cbd7
Teghino <tacchifra@gmail.com>
2023-12-04 08:26:40 +0100

    migliorato

--- client.py
+++ client.py
@@ -1,24 +1,45 @@
 import telnetlib
+import threading
 
 # Impostazioni del client
 HOST = '127.0.0.1'
 PORT = 23
 
-def run_client():
-    with telnetlib.Telnet(HOST, PORT) as client:
+def read_from_server(client):
+    while True:
         response = client.read_until(b"\n")
         print(response.decode('ascii'))
+
+def run_client():
+    with telnetlib.Telnet(HOST, PORT) as client:
+        # Fase di autenticazione
+        username = input("Nome utente: ")
+        client.write((username + "\n").encode('ascii'))
+        password = input("Password: ")
+        client.write((password + "\n").encode('ascii'))
+
+        # Attendi la risposta del server
+        response = client.read_until(b"\n").decode('ascii').strip()
+        print(response)
+
+        # Se l'accesso non è riuscito, termina il client
+        if "Accesso negato" in response:
+            return
+
+        # Avvia il thread di lettura solo dopo che la fase di registrazione è completata con successo
+        read_thread = threading.Thread(target=read_from_server, args=(client,))
+        read_thread.start()
+
+        # Fase di chat
         while True:
-            command = input("Inserisci un comando: ")
+            command = input()
             if command.strip().lower() == "exit":
                 client.write((command + "\n").encode('ascii'))
-                response = client.read_until(b"\n")
-                print("Risposta dal server:", response.decode('ascii'))
                 break
             else:
                 client.write((command + "\n").encode('ascii'))
-                response = client.read_until(b"\n")
-                print("Risposta dal server:", response.decode('ascii'))
 
+if __name__ == "__main__":
+    run_client()
 if __name__ == "__main__":
     run_client()
--- server.py
+++ server.py
@@ -1,28 +1,67 @@
 import socket
 import telnetlib
+import threading
 
-# Impostazioni del server
-HOST = '127.0.0.1'  # Indirizzo IP del server (vuoto per accettare connessioni su tutte le interfacce)
-PORT = 23  # Porta su cui il server ascolterà
+HOST = '127.0.0.1'
+PORT = 23
+accounts = [
+    {
+        "nome": "admin",
+        "psw": "admin",
+    },
+    {
+        "nome": "franci",
+        "psw": "admin",
+    },
+    {
+        "nome": "mario",
+        "psw": "admin",
+    }
+]
+
+clients = []
+
+def broadcast(message, sender):
+    for client in clients:
+        if client != sender:
+            client.write(message)
 
 def handle_client(client):
-    client.write(b"Benvenuto al server Telnet!\n")
+    clients.append(client)
     while True:
-        command = client.read_until(b"\n")  # Leggi i dati inviati dal client fino al newline
-        if command.strip().lower() == b"exit":
-            client.write(b"Arrivederci!\n")
+        try:
+            # Fase di autenticazione
+            client.write(b"Nome utente: ")
+            username = client.read_until(b"\n").decode('ascii').strip()
+            client.write(b"Password: ")
+            password = client.read_until(b"\n").decode('ascii').strip()
+
+            # Verifica delle credenziali
+            for account in accounts:
+                if account["nome"] == username and account["psw"] == password:
+                    client.write(b"Accesso riuscito. Benvenuto nella chat!\n")
+                    break
+            else:
+                client.write(b"Credenziali non valide. Accesso negato.\n")
+                clients.remove(client)
+                return
+
+            # Fase di chat
+            while True:
+                message = client.read_until(b"\n")
+                addr = client.get_socket().getpeername()
+                message_with_addr = f"{username}: {message.decode('ascii')}".encode('ascii')
+                broadcast(message_with_addr, client)
+        except:
+            clients.remove(client)
             break
-        else:
-            # Esegui l'elaborazione del comando qui
-            response = b"Hai detto: " + command
-            client.write(response + b"\n")
 
 def run_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server_socket.bind((HOST, PORT))
         server_socket.listen(5)
-        print(f"Server Telnet in ascolto su {HOST} : {PORT}")
+        print(f"Server Telnet in ascolto su {HOST}:{PORT}")
 
         while True:
             client_socket, addr = server_socket.accept()
@@ -30,8 +69,9 @@ def run_server():
             
             client = telnetlib.Telnet()
             client.sock = client_socket
-            handle_client(client)
-            client.close()
+            
+            client_thread = threading.Thread(target=handle_client, args=(client,))
+            client_thread.start()
 
 if __name__ == "__main__":
     run_server()
